38cadb1c0c5d284e233612e44f89c78e5923816b
Vyacheslav Mikhaylov <viacheslav.mikhailov@gmail.com>
2017-08-27 23:19:32 +0300

    Missing changes

---
+++ LegendDrive/Model/CountersGroup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using LegendDrive.Counters.Interfaces;
+
+namespace LegendDrive.Model
+{
+	public class CountersGroup
+	{
+		public string Name { get; set; }
+		public double Weight { get; set; } = 1;
+		public ObservableRangeCollection<IRaceCounter> Counters { get; set; }
+	}
+}
